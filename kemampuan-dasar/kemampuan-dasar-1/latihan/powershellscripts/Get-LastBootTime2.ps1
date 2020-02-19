2c0e2899c6603cc1024e7b81d3ce6dfebbcc9436
Shandy maulana <shandy1700016071@webmail.uad.ac.id>
2020-02-19 19:33:55 +0700

    second

---
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/powershellscripts/Get-LastBootTime2.ps1
@@ -0,0 +1,9 @@
+﻿param(
+[string]$ComputerName
+)
+
+Get-WmiObject -Class Win32_OperatingSystem –ComputerName $ComputerName |
+
+Select-Object -Property CSName,@{n=”Last Booted”;
+
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
